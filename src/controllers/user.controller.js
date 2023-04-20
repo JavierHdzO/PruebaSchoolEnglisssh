2000762a14f685a1183d7062e4e349142d6da143
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-04-19 18:25:03 -0600

    041923 Francisco Javier

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,25 +5,19 @@ import { hasher } from "../hepers/hash-password.js";
 
 const getUsers = async(req = request, res = response) => {
     
-    connection.query(
-        "SELECT * FROM Usuarios",
-        (err, results) => {
-
-            if(err){
-                return res.json({
-                    message: "Internal Server error",
-                    "ok":false
-                });
-            }
-
+    try {
+        const [row] = await connection.execute(
+            "SELECT * FROM Usuarios");
+        
             res.json({
-                usuarios: results,
-                ok: true
-            })
-         }
-        );
-     
-
+                users: row
+            });
+    } catch (error) {
+        return res.status(500).json({
+            message: "reporte al administrador"
+        });
+    }
+        
 }
 
 const getUser = async(req, res) => {
@@ -43,7 +37,11 @@ const getUser = async(req, res) => {
         
         )
 
-        
+        if(!row[1]){
+            res.status(404).json({
+                message:"Usuario no encontrado"
+            });
+        }
 
         res.json({
             user: row ,
@@ -64,15 +62,20 @@ const createUser = async(req, res) => {
 
         const paswordHashed = await hasher(contrasena);
         
-         await sconnection.execute(
+         const [row] = await connection.execute(
             "INSERT INTO Usuarios(Nombre, Apellidos, Usuario, Contrasena) VALUES(?,?,?,?)",
-            [nombre, apellidos, usuario, paswordHashed],
-            async (error, results) => {
-                res.json({
-                    user: results
-                })
-            }
+            [nombre, apellidos, usuario, paswordHashed]
         );
+
+
+        res.json({
+            user:{
+                nombre,
+                apellidos,
+                usuario
+            },
+            ok:true
+        });
 
     } catch (error) {
         res.status(500).json({
@@ -84,10 +87,67 @@ const createUser = async(req, res) => {
 
 const updateUser = async(req, res) => {
 
+    const { nombre, apellidos, usuario, contrasena } = req.body;
+    const { id } = req.params;
+
+    try {
+
+        if(!id) return res.status(404).json({ message: "Usuario no encotrado", ok:false  })
+
+        const paswordHashed = await hasher(contrasena);
+        
+         const [row] = await connection.execute(
+            "UPDATE Usuarios SET Nombre = ?, Apellidos = ?, Usuario = ?, Contrasena = ? WHERE UsuarioId = ?",
+            [nombre, apellidos, usuario, paswordHashed, id]
+        );
+
+        res.json({
+            user:{
+                nombre,
+                apellidos,
+                usuario
+            },
+            messsage: "Usuario actualizado",
+            ok:true
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message:"Reporte al administrador"
+        })
+    }
 }
 
 const deleteUser = async(req, res) => {
+    const { id } = req.params;
 
+    if(!id) res.status(404).json({ message: 'Usuario no encontrado', ok: false });
+
+    try {
+
+        const [row] = await connection.execute( 
+            "DELETE FROM Usuarios WHERE UsuarioId = ?",
+            [id]
+          );
+        
+
+        if(row.affectedRows === 0){
+            res.json({
+                message: "El usuario no ha podido ser eliminado"
+            });
+        }
+
+        res.json({
+            message:"El usuario ha sido eliminado",
+            ok: true
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            message:"Reporte al administrador"
+        })
+    }
 }
 
 export  {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,8 +17,8 @@ router.post("/", [
 
 router.get("/:id", getUser );
 
-router.put("/id", updateUser );
+router.put("/:id", updateUser );
 
-router.delete("/id", deleteUser );
+router.delete("/:id", deleteUser );
 
 export default router;
